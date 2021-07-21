97fa6bb6cf4aeca62dfde4cb506f50cc212db14a
Mattia <mattia@wonderflow.ai>
2021-07-21 09:08:49 +0200

    feat: add tokens types

---
+++ src/generate-types.js
@@ -0,0 +1,20 @@
+const JsonToTS = require('json-to-ts')
+const fs = require('fs-extra')
+const path = require('path')
+const tokens = require('../platforms/web/tokens.json')
+
+const run = () => {
+  let types = ''
+  JsonToTS(tokens).forEach(typeInterface => {
+    types += `\n\n${typeInterface.replace('interface', 'export interface')}`
+  })
+  fs.writeFileSync(path.join('platforms', 'web', 'types.d.ts'), types)
+}
+
+try {
+  run()
+  process.exit(0)
+} catch (error) {
+  console.error('⚠️ Something went wrong:', error)
+  process.exit(1)
+}
