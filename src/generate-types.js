d5c2f62def59a7c314df68024ff166d4b69400d2
Mattia <mattia@wonderflow.ai>
2021-07-21 13:57:40 +0200

    fix: update token types format

--- src/generate-types.js
+++ src/generate-types.js
@@ -1,15 +1,31 @@
-const JsonToTS = require('json-to-ts')
+// const JsonToTS = require('json-to-ts')
 const fs = require('fs-extra')
 const path = require('path')
-const tokens = require('../platforms/web/tokens.json')
+
+const deeperKeys = ['color', 'font', 'icon']
+
+const printCorrectValue = value => (Number.isNaN(Number(value)) ? `'${value}'` : value)
+const getTypeUnion = json => Object.keys(json).reduce(
+  (acc, key, index) => acc.concat(`${index !== 0 ? '| ' : ''}${printCorrectValue(key)} `), ''
+)
+
+const reduceTokensJson = (tokens) => {
+  return Object.keys(tokens).reduce((acc, key) => {
+    const jsonEntity = tokens[key]
+
+    if (deeperKeys.includes(key)) {
+      return acc.concat(`${key}: { ${reduceTokensJson(jsonEntity)} };\n`)
+    } else {
+      const allKeysAsTypeUnion = getTypeUnion(jsonEntity)
+      return allKeysAsTypeUnion ? acc.concat(`${key}: ${allKeysAsTypeUnion.trimEnd()};\n `) : acc
+    }
+  }, '')
+}
 
 const run = () => {
-  let types = ''
-  JsonToTS(tokens, {
-    rootName: 'TokensType'
-  }).forEach(typeInterface => {
-    types += `\n\n${typeInterface.replace('interface', 'export type').replace('{', '= {')}`
-  })
+  const tokens = require('../platforms/web/tokens.json')
+  const types = `export type TokensTypes = {\n ${reduceTokensJson(tokens)} \n}`
+
   fs.writeFileSync(path.join('platforms', 'web', 'types.d.ts'), types)
 }
 
